fa51e42e17a368ac2b842b4a0e467d88a65c483b
SeoJin <sjinjin7@gmail.com>
2025-08-06 00:44:49 +0900

    8. MVC와 템플릿 엔진

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,6 +12,18 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    /**
+     * 8. MVC와 템플릿 엔진
+     * 관심사의 분리라는 원칙으로 각각의 관심사가 명확히 구분한 형태가 MVC
+     *
+     * ctrl + p 파라미터 정보
+     */
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 
 }
